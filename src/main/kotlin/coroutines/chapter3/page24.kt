09aa1fe755da6e074a1c7f8be7552ed7f550cdb5
roger <jmirs83@gmail.com>
2023-11-17 12:39:57 +0900

    kotlin coroutines 책 3장 "중단은 어떻게 작동할까?"
    - 24~26페이지
    - suspendCoroutine 학습

--- src/main/kotlin/coroutines/chapter3/page24.kt
+++ src/main/kotlin/coroutines/chapter3/page24.kt
@@ -1,13 +1,23 @@
 package coroutines.chapter3
 
+import kotlin.concurrent.thread
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
 suspend fun main() {
   println("Before")
 
   suspendCoroutine<Unit> { continuation ->
-    println("Before too")
+    continueAfterSecond(continuation)
   }
 
   println("After")
+}
+
+fun continueAfterSecond(continuation: Continuation<Unit>) {
+  thread {
+    Thread.sleep(1_000L)
+    continuation.resume(Unit)
+  }
 }
