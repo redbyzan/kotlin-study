f3acf1c19d413da5ed11201294147fd21fdd97ed
roger <jmirs83@gmail.com>
2023-11-17 12:40:41 +0900

    kotlin coroutines 책 3장 "중단은 어떻게 작동할까?"
    - 27~28 페이지
    - threadExecutor 활용

---
+++ src/main/kotlin/coroutines/chapter3/page27.kt
@@ -0,0 +1,22 @@
+package coroutines.chapter3
+
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+private val executor = Executors.newSingleThreadScheduledExecutor() {
+  Thread(it, "scheduler").apply { isDaemon = true }
+}
+
+suspend fun main() {
+  println("Before")
+
+  suspendCoroutine<Unit> { continuation ->
+    executor.schedule({
+      continuation.resume(Unit)
+    }, 1_000, TimeUnit.MILLISECONDS)
+  }
+
+  println("After")
+}
